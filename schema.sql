c4a81f5ad15d0cb337dd92465e75319234cf4ef1
alicegcr <alicegcr@gmail.com>
2022-10-04 16:04:19 -0700

    Add new csv

--- schema.sql
+++ schema.sql
@@ -3,7 +3,7 @@ CREATE TABLE departments (
      dept_no VARCHAR(4) NOT NULL,
      dept_name VARCHAR(40) NOT NULL,
      PRIMARY KEY (dept_no),
-     UNIQUE (dept_name)
+	 UNIQUE (dept_name)
 );
 CREATE TABLE employees (
      emp_no INT NOT NULL,
@@ -31,7 +31,7 @@ CREATE TABLE salaries (
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no)
 );
-CREATE TABLE demt_emp (
+CREATE TABLE dept_emp (
   emp_no INT NOT NULL,
   dept_no VARCHAR(4) NOT NULL,
   from_date DATE NOT NULL,
@@ -49,3 +49,62 @@ CREATE TABLE titles (
   PRIMARY KEY (emp_no)
 );
 SELECT * FROM departments;
+
+SELECT * FROM departments;
+
+DROP TABLE demt_emp CASCADE;
+DROP TABLE dept_emp CASCADE;
+
+CREATE TABLE dept_emp (
+  emp_no INT NOT NULL,
+dept_no VARCHAR(4) NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+  PRIMARY KEY (emp_no)
+);
+
+DROP TABLE dept_emp CASCADE;
+
+CREATE TABLE dept_emp (
+  emp_no INT NOT NULL,
+  dept_no VARCHAR(4) NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+  PRIMARY KEY (emp_no)
+);
+DROP TABLE dept_emp CASCADE;
+
+CREATE TABLE dept_emp (
+  emp_no INT NOT NULL,
+  dept_no VARCHAR(4) NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+  PRIMARY KEY (emp_no,dept_no)
+);
+
+DROP TABLE titles CASCADE;
+
+CREATE TABLE titles (
+  emp_no INT NOT NULL,
+  title VARCHAR NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no,title)
+);
+
+DROP TABLE titles CASCADE;
+
+CREATE TABLE titles (
+  emp_no INT NOT NULL,
+  title VARCHAR NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+);
